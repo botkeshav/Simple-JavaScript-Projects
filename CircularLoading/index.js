8a523ffb8534079ac24063e3a4674f17c9edec4d
Keshav <botkeshav@gmail.com>
2025-10-02 23:55:34 +0530

    Added Logic for the following app

---
+++ CircularLoading/index.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+
+
+const inner = document.querySelector(".inner");
+const h1 = document.querySelector(".inner h1");
+const inner_width = inner.clientWidth;
+
+console.log(inner_width);
+
+
+// document.addEventListener("onload")
+
+window.addEventListener('load',()=>{
+    console.log("Hellow ");
+    
+})
+
+
