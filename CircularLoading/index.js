dae4b9bbe7dd0be0b352129870cf6c828d4d1809
Keshav <botkeshav@gmail.com>
2025-10-03 23:48:41 +0530

    Fixed loading logic

--- CircularLoading/index.js
+++ CircularLoading/index.js
@@ -1,19 +1,25 @@
 console.log("Hello");
-
-
-
 const inner = document.querySelector(".inner");
 const h1 = document.querySelector(".inner h1");
-const inner_width = inner.clientWidth;
+const inner_width = outer.clientWidth;
 
 console.log(inner_width);
 
 
-// document.addEventListener("onload")
-
 window.addEventListener('load',()=>{
-    console.log("Hellow ");
+
+    let i =0;
+    inner.style.width = "0px"; 
+
+  function animate() {
+    if (i <= inner_width) {
+      inner.style.width = i + "px";
+    //   i++;
+        i += 2;
+      requestAnimationFrame(animate); // schedule next frame
+    }
+  }
+
+  animate();
     
 })
-
-
