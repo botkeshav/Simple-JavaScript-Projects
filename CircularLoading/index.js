ff7587669a1c89380e159f2a3a4b34aa678b9351
Keshav <botkeshav@gmail.com>
2025-10-04 22:59:41 +0530

    Changed some designs and added logic relative to that

--- CircularLoading/index.js
+++ CircularLoading/index.js
@@ -1,25 +1,23 @@
 console.log("Hello");
+
+const outer = document.querySelector(".outer"); // added this
 const inner = document.querySelector(".inner");
 const h1 = document.querySelector(".inner h1");
-const inner_width = outer.clientWidth;
 
+const inner_width = outer.clientWidth; // target width
 console.log(inner_width);
 
-
-window.addEventListener('load',()=>{
-
-    let i =0;
-    inner.style.width = "0px"; 
+window.addEventListener('load', () => {
+  let i = 0;
+  inner.style.width = "0px"; // start small
 
   function animate() {
     if (i <= inner_width) {
       inner.style.width = i + "px";
-    //   i++;
-        i += 2;
-      requestAnimationFrame(animate); // schedule next frame
+      i += 2; // speed
+      requestAnimationFrame(animate);
     }
   }
 
   animate();
-    
-})
+});
